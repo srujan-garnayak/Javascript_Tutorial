361748c455fd799acdbc088114f0532c412b8ce0
srujan-garnayak <srujangarnayak@gmail.com>
2025-02-10 22:51:17 +0530

    completed part 1 arrays and loops

--- ToDo List/ToDo-List.js
+++ ToDo List/ToDo-List.js
@@ -1,4 +1,21 @@
-const todoList = [];
+const todoList = ['make dinner', 'wash dishes'];
+
+renderToolList();
+
+function renderToolList() {
+
+    let todoListHTML = '';
+
+    for (let i = 0; i < todoList.length; i++) {
+        const todo = todoList[i];
+        const html = `<p>${todo}</p>`;
+        todoListHTML += html;
+    }
+    console.log(todoListHTML);
+
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+}
 
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input')
@@ -9,4 +26,5 @@ function addTodo() {
 
     inputElement.value = '';
 
+    renderToolList();
 }
